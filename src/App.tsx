45b75807cc846014c61ecc285dc5f12e9a945bdd
Mehdi Amokrane <MAmokrane.prestataire@oui.sncf>
2022-04-25 13:49:33 +0200

    install and configure emotion for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Counter } from './Counter'
+import * as styles from './styles'
 
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1 css={styles.test}>React TypeScript Webpack Starter Template</h1>
       <Counter />
     </>
   )
